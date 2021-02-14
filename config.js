44ee0321d7f4e45d5e1bf1214aa8e7522e7b570b
Omid <>
2021-02-14 12:20:15 +0100

    config

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 module.exports = {
   url: 'https://lumen.netlify.com',
   pathPrefix: '/',
-  title: 'Blog by John Doe',
+  title: 'امید حق‌بین',
   subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
   copyright: '© All rights reserved.',
   disqusShortname: '',
